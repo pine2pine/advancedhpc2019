2fb2533d4a0944756eeda0f9c80b0a558acef6a6
pine2pine <doannhutung@gmail.com>
2019-10-31 20:36:22 +0700

    New version of report, src

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -109,7 +109,19 @@ void Labwork::labwork1_CPU() {
 void Labwork::labwork1_OpenMP() {
     int pixelCount = inputImage->width * inputImage->height;
     outputImage = static_cast<char *>(malloc(pixelCount * 3));
-    // do something here
+
+    // Parallel processing part (copied from the normal code)
+
+    #pragma omp parallel for schedule(dynamic)
+
+    for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
+        for (int i = 0; i < pixelCount; i++) {
+            outputImage[i * 3] = (char) (((int) inputImage->buffer[i * 3] + (int) inputImage->buffer[i * 3 + 1] +
+                                          (int) inputImage->buffer[i * 3 + 2]) / 3);
+            outputImage[i * 3 + 1] = outputImage[i * 3];
+            outputImage[i * 3 + 2] = outputImage[i * 3];
+        }
+    }
 }
 
 int getSPcores(cudaDeviceProp devProp) {
